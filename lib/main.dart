11fbcee656edb42e40bacdfcf668ac7de57df18f
ccleren <91672739+ccleren@users.noreply.github.com>
2025-05-07 20:50:54 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
-
+import 'routes/app_routes.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -17,62 +18,14 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Reserva de Salas',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
         useMaterial3: true,
       ),
-      home: const RoomsScreen(), // 👈 aquí cargamos nuestra pantalla de salas
+      initialRoute: AppRoutes.rooms,
+      routes: AppRoutes.routes,
     );
   }
 }
 
-class RoomsScreen extends StatelessWidget {
-  const RoomsScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    final roomsRef = FirebaseFirestore.instance.collection('rooms');
-
-    return Scaffold(
-      appBar: AppBar(title: const Text('Salas disponibles')),
-      body: StreamBuilder<QuerySnapshot>(
-        stream: roomsRef.snapshots(),
-        builder: (context, snapshot) {
-          if (snapshot.hasError) {
-            return Center(child: Text('❌ Error: ${snapshot.error}'));
-          }
-
-          if (!snapshot.hasData) {
-            return const Center(child: CircularProgressIndicator());
-          }
-
-          final docs = snapshot.data!.docs;
-
-          if (docs.isEmpty) {
-            return const Center(child: Text('⚠️ No hay salas disponibles'));
-          }
-
-          return ListView.builder(
-            itemCount: docs.length,
-            itemBuilder: (context, index) {
-              final room = docs[index];
-              final data = room.data() as Map<String, dynamic>;
-
-              return ListTile(
-                title: Text(
-                  data.containsKey('name') ? data['name'] : 'Sala sin nombre',
-                ),
-                subtitle: Text(
-                  data.containsKey('capacity')
-                      ? 'Capacidad: ${data['capacity']}'
-                      : 'Capacidad desconocida',
-                ),
-                trailing: const Icon(Icons.add),
-              );
-            },
-          );
-        },
-      ),
-    );
-  }
-}
